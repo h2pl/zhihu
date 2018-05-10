883d209cc6e884581070e0c25f69ae34f259b701
724888 <362294931@qq.com>
2018-05-10 14:33:52 +0800

    知乎：新增评论和站内信功能。

---
+++ src/main/java/com/nowcoder/service/MessageService.java
@@ -0,0 +1,37 @@
+package com.nowcoder.service;
+
+import com.nowcoder.dao.MessageDAO;
+import com.nowcoder.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by nowcoder on 2016/7/9.
+ */
+@Service
+public class MessageService {
+    @Autowired
+    MessageDAO messageDAO;
+
+    @Autowired
+    SensitiveService sensitiveService;
+
+    public int addMessage(Message message) {
+        message.setContent(sensitiveService.filter(message.getContent()));
+        return messageDAO.addMessage(message);
+    }
+
+    public List<Message> getConversationDetail(String conversationId, int offset, int limit) {
+        return messageDAO.getConversationDetail(conversationId, offset, limit);
+    }
+
+    public List<Message> getConversationList(int userId, int offset, int limit) {
+        return messageDAO.getConversationList(userId, offset, limit);
+    }
+
+    public int getConvesationUnreadCount(int userId, String conversationId) {
+        return messageDAO.getConvesationUnreadCount(userId, conversationId);
+    }
+}
